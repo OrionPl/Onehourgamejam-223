586866f59cde6b883ce8009e9987bf471a4cad89
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2019-08-03 22:25:52 +0200

    new proj

---
+++ Onehourgamejam 223/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float moveSpeed = 1;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void Movement()
+    {
+        int horizontal = 0;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontal = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontal = 1;
+        }
+
+        _rb.velocity = new Vector2(horizontal * moveSpeed, _rb.velocity.y);
+    }
+}
