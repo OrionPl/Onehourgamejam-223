1f71832901e36805cdd72fd3614adf0f90782e2e
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2019-08-03 22:58:13 +0200

    onground

--- Onehourgamejam 223/Assets/Scripts/PlayerController.cs
+++ Onehourgamejam 223/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float moveSpeed = 1;
+    public bool onGround = true;
+    public float jumpForce = 1;
 
     void Start()
     {
@@ -33,9 +35,24 @@ public class PlayerController : MonoBehaviour
 
         _rb.velocity = new Vector2(horizontal * moveSpeed, _rb.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
-        {
+        CheckForGround();
 
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && onGround)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+            onGround = false;
+        }
+    }
+
+    private void CheckForGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0, -1.5f), Vector2.down);
+
+        Debug.Log(hit.collider.gameObject.name);
+
+        if (hit.collider != null)
+        {
+            onGround = true;
         }
     }
 }
